bba6c0b186f398671a3b4d0b922ce423215c35c7
unknown <ibmjavanh16@iiht.tech>
2021-03-25 17:25:42 +0530

    changes made

--- src/main/java/com/ibm/demo/OrderController.java
+++ src/main/java/com/ibm/demo/OrderController.java
@@ -2,13 +2,17 @@ package com.ibm.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ibm.demo.entity.order;
+import com.ibm.demo.entity.Order;
 import com.ibm.demo.service.OrderService;
+
+import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,30 +20,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 @RestController
 public class OrderController {//frontend
 	@Autowired
 	OrderService orderService;//DI
             @PostMapping("/order")
-            String createOrder(@RequestBody@Valid order orders, BindingResult bindingResult) {
-            	if(bindingResult.hasErrors()) {
-            		throw new IllegalArgumentException("something went wrong");
-            	}
-            	System.out.println(orders);
-	             return orderService.createOrder(orders);
+            @ResponseStatus(code = HttpStatus.CREATED)
+            String createOrder(@RequestBody@Valid Order order, BindingResult bindingResult) {
+            	validateModel(bindingResult);
+            	System.out.println(order);
+	             return orderService.createOrder(order);
 }
-            @GetMapping("/order")
-            String getOrder() {
-            	return "order created";
+            @GetMapping("/order/{id}")
+            Order getOrder(@PathVariable("id") int orderId) {
+            	return orderService.getOrder(orderId);
+            }
+            @GetMapping("/order}")
+            List<Order> getOrders() {
+            	return orderService.getOrders();
+            }
+            
+            //DRY
+            private void validateModel(Errors bindingResult) {
+            	if(bindingResult.hasErrors()) {
+             		throw new IllegalArgumentException("something went wrong, please try again");
+             	}
             }
          @PutMapping("/order/{id}")
-         String updateOrder(@PathVariable("id") int orderId){
+         void updateOrder(@RequestBody@Valid Order order, @PathVariable("id") int orderId, BindingResult bindingResult){
+        	 validateModel(bindingResult);
         	 System.out.println(orderId);
-        	 return "order updated";
+        	 orderService.updateOrder(orderId);
          }
          @DeleteMapping("/order/{id}")
-         String deleteOrder(@PathVariable("id") int orderId) {
+         void deleteOrder(@PathVariable("id") int orderId) {
         	 System.out.println(orderId);
-        	 return "order delete";
+        	 orderService.deleteOrder();
          }
 }
--- src/main/java/com/ibm/demo/service/OrderService.java
+++ src/main/java/com/ibm/demo/service/OrderService.java
@@ -1,12 +1,35 @@
 package com.ibm.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import com.ibm.demo.entity.order;
+import com.ibm.demo.entity.Order;
 
 @Service
 public class OrderService {
-public String createOrder(order orders) {
+public String createOrder(Order orders) {
 	return "order created";
 }
+
+public Order getOrder(int orderId) {
+	
+	return new Order();
+}
+public List<Order> getOrders(){
+	return new ArrayList<Order>();
+}
+
+public void updateOrder(int orderId) {
+	
+	
+}
+
+public void deleteOrder() {
+	
+	System.out.println("order deleted");
+}
+
+
 }
